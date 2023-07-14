c0679060efd604f295b9020744d01fca7a9af7fb
Gustavo Rosa <gustavorib.rosa@gmail.com>
2023-07-13 21:59:34 -0300

    Initial commit

---
+++ Data/MVCPostgresContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using MVCPostgres.Models;
+
+namespace MVCPostgres.Data
+{
+    public class MVCPostgresContext : DbContext
+    {
+        public MVCPostgresContext (DbContextOptions<MVCPostgresContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<MVCPostgres.Models.Department> Department { get; set; } = default!;
+    }
+}
