0413d6b44116c12dea9f35b626a3ffc50ac60d00
Gustavo Rosa <gustavorib.rosa@gmail.com>
2023-07-14 18:21:25 -0300

    postgres setup and first migration

--- Data/MVCPostgresContext.cs
+++ Data/MVCPostgresContext.cs
@@ -4,16 +4,49 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.EntityFrameworkCore;
 using MVCPostgres.Models;
+using Microsoft.Extensions.Configuration;
+
+//namespace MVCPostgres.Data
+//{
+//    public class MVCPostgresContext : DbContext
+//    {
+//        public MVCPostgresContext (DbContextOptions<MVCPostgresContext> options)
+//            : base(options)
+//        {
+//        }
+
+//        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+//        {
+//            base.OnConfiguring(optionsBuilder);
+//            // connect to postgres with connection string from app settings
+//            optionsBuilder.UseNpgsql(Configuration.GetConnectionString("MVCPostgresContext"));
+//        }
+
+//        public DbSet<Department> Department { get; set; } = default!;
+//    }
+//}
+
+
+//using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
 
 namespace MVCPostgres.Data
 {
     public class MVCPostgresContext : DbContext
     {
-        public MVCPostgresContext (DbContextOptions<MVCPostgresContext> options)
-            : base(options)
+        protected readonly IConfiguration Configuration;
+
+        public MVCPostgresContext(IConfiguration configuration)
         {
+            Configuration = configuration;
         }
 
-        public DbSet<MVCPostgres.Models.Department> Department { get; set; } = default!;
+        protected override void OnConfiguring(DbContextOptionsBuilder options)
+        {
+            // connect to postgres with connection string from app settings
+            options.UseNpgsql(Configuration.GetConnectionString("MVCPostgresContext"));
+        }
+
+        public DbSet<Department> Department { get; set; }
     }
 }
